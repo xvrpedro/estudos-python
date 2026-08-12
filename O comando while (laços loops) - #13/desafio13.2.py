2bbb6620d3bbe7a04bbcc36a2e2b5c32faaf4338
Pedro Xavier <arclimatizadot@gmail.com>
2026-08-12 15:41:26 -0300

    Aula 13: comando while (laços de repetição)

---
+++ O comando while (laços loops) - #13/desafio13.2.py
@@ -0,0 +1,9 @@
+i = 1
+
+while i != 0:
+    i = int(input("Digite um número (0 para parar): "))
+    if i != 0:
+        print(f"Número digitado: {i}")
+    elif i == 0:
+        print("Encerrado.")
+    
