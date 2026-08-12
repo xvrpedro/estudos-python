b4cf6a81140c370b91d86a1c757dbb407021389f
Pedro Xavier <arclimatizadot@gmail.com>
2026-08-12 15:41:32 -0300

    Aula 14: comando for e função range() (laços de repetição)

---
+++ O comando for + função range() (laços loops) - #14/Ex_14.py
@@ -0,0 +1,7 @@
+'''
+i = 1 | while i < 11 | i += 1
+se não for informado o valor inicial, por padrão assume-se 0
+se a gente não informar o passo, assume-se por padrão o valor 1
+'''
+for i in range (1, 11):
+    print(i, " ", end="")
---
+++ O comando for + função range() (laços loops) - #14/desafio14.py
@@ -0,0 +1,3 @@
+# i = 10 | while i > 0 | i -= 1
+for i in range(10, 0, -1):
+    print(i, " ", end="")
