3acbe82ad2853f82e5168a45db58b217711c3a60
Pedro Xavier <arclimatizadot@gmail.com>
2026-08-14 19:02:51 -0300

    Aula 19: tuplas (tuples)

---
+++ Tuplas (tuples) - #19/Ex_19.py
@@ -0,0 +1,8 @@
+tupla = (0, 1, 3, 3, 5, 7, 7, 7)
+
+print(
+    f"Conteúdo: {tupla}\n"
+    f"Quantidade de elementos: {len(tupla)}\n"
+    f"Quantidade de elementos iguais a 7: {tupla.count(7)}\n"
+    f"Elemento da posição 4: {tupla[4]}"
+)
---
+++ Tuplas (tuples) - #19/desafio19.py
@@ -0,0 +1,6 @@
+frutas = ("maçã", "banana", "laranja")
+
+for i in frutas:
+    print(i, end=" ")
+
+print(f"\nQuantidade de elementos na tupla: {len(frutas)}")
